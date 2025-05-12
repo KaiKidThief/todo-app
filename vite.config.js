9a8ffa8d75c24d359eb5d8ef3af35b8f23ad2398
KaiKidThief <roxas.269269@meycauayan.sti.edu.ph>
2025-05-12 18:56:38 +0800

    ADD

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/todo-app",
+  base: "/todo-app/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
+  },
 });
